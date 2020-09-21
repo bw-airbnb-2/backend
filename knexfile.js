d195fb0c54fd4e4fbb96da89ee1ed2537c24d789
Chris Harwell <chrisharwell72@gmail.com>
2020-09-21 13:39:38 -0400

    added testing and production to knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,5 @@
+const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhost/airbnb";
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -5,8 +7,37 @@ module.exports = {
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
-      tableName: 'dbmigrations',
+      tableName: 'users',
     },
     seeds: { directory: './database/seeds' },
+  },
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/test.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations",
+      tableName: 'users',
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: pgConnection,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
   },
 };
