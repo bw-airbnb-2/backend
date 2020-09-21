f9cafbdba443d169d977544e8295b7c32b9bca04
Chris Harwell <chrisharwell72@gmail.com>
2020-09-21 14:19:10 -0400

    maybe fixed heroku error again?

--- api/server.js
+++ api/server.js
@@ -12,12 +12,13 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
-server.get("/", (_, res) => {
-    res.status(200).json({ api: "up" });
-  });
+
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 server.use('/api/users',authenticate, userRouter);
 
+server.get("/", (_, res) => {
+    res.status(200).json({ api: "up" });
+  });
 module.exports = server;
