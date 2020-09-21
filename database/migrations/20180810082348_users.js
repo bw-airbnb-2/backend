8a1a7c5e831ef41bd03ce9638b8c9edca2bb50d6
Chris Harwell <chrisharwell72@gmail.com>
2020-09-21 18:15:59 -0400

    added listings table to migrations for test db

--- database/migrations/20180810082348_users.js
+++ database/migrations/20180810082348_users.js
@@ -1,15 +1,36 @@
-exports.up = function(knex) {
-  return knex.schema.createTable('users', users => {
-    users.increments();
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("users", (users) => {
+      users.increments();
 
-    users
-      .string('username', 255)
-      .notNullable()
-      .unique();
-    users.string('password', 255).notNullable();
-  });
+      users.string("username", 255).notNullable().unique();
+      users.string("password", 255).notNullable();
+    })
+    .createTable("listings", (tbl) => {
+      tbl.increments();
+      tbl
+        .integer("userId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl
+        .integer("name")
+        .unsigned()
+        .notNullable()
+        .references("username")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl.string("room_type", 255).notNullable();
+      tbl.integer("min_num_nights").notNullable();
+      tbl.string("location", 100).notNullable();
+      tbl.float("price", 2).notNullable();
+    })
 };
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('users');
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("listings");
 };
