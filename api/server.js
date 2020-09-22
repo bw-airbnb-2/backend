e67d07472055aa1e82865f6a9fb5659623a10c7f
Chris Harwell <chrisharwell72@gmail.com>
2020-09-22 14:08:43 -0400

    .

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', authenticate, userRouter);
-server.use('/api', authRouter, authenticate, listingsRouter);
+server.use('/api', authenticate, listingsRouter);
 
 server.get("/", (_, res) => {
     res.status(200).json({ api: "up" });
